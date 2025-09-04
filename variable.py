14e6702d3c4a09a436172ccd707a5a4e0c787287
SalmanLiaqat3 <129200506+SalmanLiaqat3@users.noreply.github.com>
2025-09-04 22:38:57 +0500

    first commit

---
+++ variable.py
@@ -0,0 +1,13 @@
+# Variable naming Convention: 
+'''
+1.camel case: i.e; myName
+2.pascal case:i.e; MyName
+3.snake case:i.e; my_name
+'''
+# Don't use these when you declear variable
+'''
+1.Num. at the start
+2.special characters
+3.spaces b/w the name
+
+'''
