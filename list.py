742fe675ce8dda07fdb54cc8c7f2ee6def7fadfd
SalmanLiaqat3 <129200506+SalmanLiaqat3@users.noreply.github.com>
2025-09-14 04:36:55 +0500

    Modify File

--- list.py
+++ list.py
@@ -19,7 +19,52 @@ print(f"The maximum number is {max}")
 #Methode
 
 # Append
-
+'''
 a = [23,56,48,78]
 a.append(45)
 print(a)
+'''
+
+# Insert
+'''
+n =[1,2,3,4,5]
+
+n.insert(2,23)
+
+print(n)
+
+'''
+
+# Remove
+'''
+a = [23,56,48,78]
+a.remove(48)
+print(a)
+'''
+
+#extends
+'''
+n =[1,2,3,4,5,6]
+n.extend([23,43,56])
+print(n)
+'''
+
+#index
+'''
+n =[1,2,3,4,5,6]
+index =n.index(4)
+print(index)
+'''
+
+# Reseverse
+
+n =[1,2,2,3,4,5,6]
+n.reverse()
+print(n)
+#sort
+n.sort()
+print(n)
+
+#count
+count= n.count(2)
+print(count)
