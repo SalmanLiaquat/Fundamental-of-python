90e94bed68ba8e4103fa7606c4e604d102d1775d
SalmanLiaqat3 <129200506+SalmanLiaqat3@users.noreply.github.com>
2025-09-13 20:43:10 +0500

    Add  File

---
+++ list.py
@@ -0,0 +1,19 @@
+# List → a collection of items
+'''
+→ mutaqbl
+→ duplicate
+→ hetergenous
+→ ordered
+'''
+'''
+a =[23,56,48,78]
+max =0
+for i in range(len(a)):
+   for j in range(len(a)):
+      if a[i]>a[j]:
+         max=a[i]
+         
+print(f"The maximum number is {max}") 
+'''
+
+#Methode
