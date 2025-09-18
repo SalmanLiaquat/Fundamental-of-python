07eb4ef685512a79e52b2bbb25a19a793899bb63
SalmanLiaqat3 <129200506+SalmanLiaqat3@users.noreply.github.com>
2025-09-18 09:41:45 +0500

    Add File

---
+++ tuple.py
@@ -0,0 +1,19 @@
+ # tuple → a collection of items
+'''
+→ immutable
+→ duplicate
+→ hetergenous
+→ ordered
+'''
+# Methode are just 2 → index finding → count 
+tup =(123,43,56,47)
+index =tup.index(123)
+count =tup.count(123)
+print(index)
+print(count)
+        
+     
+ #TUPLE AND INTERGER DIFFERENCE
+
+tup =(1) #THAT IS CONSIDER AS AN INTEGER
+tup =(1,) #THAT IS CONSIDER AS A TUPLE
