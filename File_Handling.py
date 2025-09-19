8b283a60e492cdc0e867e5831e3f96470c1b2924
SalmanLiaqat3 <129200506+SalmanLiaqat3@users.noreply.github.com>
2025-09-19 18:19:56 +0500

    Add File

---
+++ File_Handling.py
@@ -0,0 +1,17 @@
+# we use → open() → function to open the file
+# Syntax: open(file_name)
+
+# read
+'''
+f = open("dictionary.py")
+print(f.read())
+f.close
+
+'''
+
+# write w→ write
+# append a→ append
+
+f =open("superman.txt",'a')
+f.write("\nI am workng as an AI  engineer")
+f.close
